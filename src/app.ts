4cd84d0daba80d551dd270d4913dea9d05db47fd
kmkzt <info.pscreator@gmail.com>
2020-08-23 21:46:52 +0900

    feat: Colorful box render

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { mat4 } from 'gl-matrix'
+// Fragment shader program
+import fsSource from './color.frag'
 // Vertex shader program
 import vsSource from './square.vert'
-// Fragment shader program
-import fsSource from './white.frag'
 
 //
 // Start here
@@ -14,23 +14,28 @@ function main() {
   // If we don't have a GL context, give up now
 
   if (!gl) {
-    alert(
+    console.error(
       'Unable to initialize WebGL. Your browser or machine may not support it.'
     )
     return
   }
-
   // Initialize a shader program; this is where all the lighting
   // for the vertices and so forth is established.
   const shaderProgram = initShaderProgram(gl, vsSource, fsSource)
 
+  if (!shaderProgram) {
+    console.error('[ERROR]  initShaderProgram')
+    return
+  }
   // Collect all the info needed to use the shader program.
-  // Look up which attribute our shader program is using
-  // for aVertexPosition and look up uniform locations.
+  // Look up which attributes our shader program is using
+  // for aVertexPosition, aVevrtexColor and also
+  // look up uniform locations.
   const programInfo = {
     program: shaderProgram,
     attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor')
     },
     uniformLocations: {
       projectionMatrix: gl.getUniformLocation(
@@ -66,7 +71,6 @@ function initBuffers(gl: WebGLRenderingContext) {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
   // Now create an array of positions for the square.
-
   const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0]
 
   // Now pass the list of positions into WebGL to build the
@@ -75,15 +79,45 @@ function initBuffers(gl: WebGLRenderingContext) {
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
+  // Now set up the colors for the vertices
+
+  const colors = [
+    // white
+    1.0,
+    1.0,
+    1.0,
+    1.0,
+    // red
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    // green
+    0.0,
+    1.0,
+    0.0,
+    1.0,
+    // blue
+    0.0,
+    0.0,
+    1.0,
+    1.0
+  ]
+
+  const colorBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
+
   return {
-    position: positionBuffer
+    position: positionBuffer,
+    color: colorBuffer
   }
 }
 
 //
 // Draw the scene.
 //
-function drawScene(gl: any, programInfo: any, buffers: any) {
+function drawScene(gl: WebGLRenderingContext, programInfo: any, buffers: any) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0) // Clear to black, fully opaque
   gl.clearDepth(1.0) // Clear everything
   gl.enable(gl.DEPTH_TEST) // Enable depth testing
@@ -101,7 +135,10 @@ function drawScene(gl: any, programInfo: any, buffers: any) {
   // and 100 units away from the camera.
 
   const fieldOfView = (45 * Math.PI) / 180 // in radians
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
+  const aspect =
+    gl.canvas instanceof HTMLCanvasElement
+      ? gl.canvas.clientWidth / gl.canvas.clientHeight
+      : 1
   const zNear = 0.1
   const zFar = 100.0
   const projectionMatrix = mat4.create()
@@ -124,7 +161,7 @@ function drawScene(gl: any, programInfo: any, buffers: any) {
   ) // amount to translate
 
   // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
+  // buffer into the vertexPosition attribute
   {
     const numComponents = 2
     const type = gl.FLOAT
@@ -141,6 +178,26 @@ function drawScene(gl: any, programInfo: any, buffers: any) {
       offset
     )
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition)
+  }
+
+  // Tell WebGL how to pull out the colors from the color buffer
+  // into the vertexColor attribute.
+  {
+    const numComponents = 4
+    const type = gl.FLOAT
+    const normalize = false
+    const stride = 0
+    const offset = 0
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color)
+    gl.vertexAttribPointer(
+      programInfo.attribLocations.vertexColor,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    )
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor)
   }
 
   // Tell WebGL to use our program when drawing
@@ -170,21 +227,32 @@ function drawScene(gl: any, programInfo: any, buffers: any) {
 //
 // Initialize a shader program, so WebGL knows how to draw our data
 //
-function initShaderProgram(gl: any, vsSource: any, fsSource: any) {
+function initShaderProgram(
+  gl: WebGLRenderingContext,
+  vsSource: string,
+  fsSource: string
+): WebGLProgram | null {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
 
+  if (!vertexShader || !fragmentShader) {
+    console.error('[ERROR] loadShader error/')
+    return null
+  }
   // Create the shader program
-
   const shaderProgram = gl.createProgram()
+  if (!shaderProgram) {
+    console.error('[ERROR] shaderProgram is null.')
+    return null
+  }
   gl.attachShader(shaderProgram, vertexShader)
   gl.attachShader(shaderProgram, fragmentShader)
   gl.linkProgram(shaderProgram)
 
-  // If creating the shader program failed, alert
+  // If creating the shader program failed, console.error
 
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert(
+    console.error(
       'Unable to initialize the shader program: ' +
         gl.getProgramInfoLog(shaderProgram)
     )
@@ -198,23 +266,26 @@ function initShaderProgram(gl: any, vsSource: any, fsSource: any) {
 // creates a shader of the given type, uploads the source and
 // compiles it.
 //
-function loadShader(gl: any, type: any, source: any) {
+function loadShader(
+  gl: WebGLRenderingContext,
+  type: GLenum,
+  source: string
+): WebGLShader | null {
   const shader = gl.createShader(type)
+  if (!shader) {
+    console.error('[ERROR] invalid Shader type number.')
+    return null
+  }
 
   // Send the source to the shader object
-
   gl.shaderSource(shader, source)
 
   // Compile the shader program
-
   gl.compileShader(shader)
 
   // See if it compiled successfully
-
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(
-      'An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader)
-    )
+    console.error('[ERROR] compileinfo: ' + gl.getShaderInfoLog(shader))
     gl.deleteShader(shader)
     return null
   }
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,3 +1,9 @@
 // file extension
-declare module '*.vert'
-declare module '*.frag'
+declare module '*.vert' {
+  const vert: string
+  export default vert
+}
+declare module '*.frag' {
+  const frag: string
+  export default frag
+}
